2815e44cbff62de9cee08b6d5eaf933d5458fde8
Megan Horsfield <meganthadev@gmail.com>
2020-03-02 23:25:49 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,17 @@
 class Waiter
 
+ attr_accessor :name, :age 
+  
+  @@all = []
+  
+  def initialize(name, age) 
+    @name = name
+    @age = age
+    @@all << self 
+  end   
+  
+  def self.all
+    @@all
+  end
+
 end
